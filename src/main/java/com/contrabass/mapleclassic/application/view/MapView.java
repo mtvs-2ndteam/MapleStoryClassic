de84a97e5deb02f61501146964a3c2a64ea5b74b
manje cho <whakswp123@naver.com>
2023-06-15 19:44:46 +0900

    마을 출력 메소드 추가 (#2)

---
+++ src/main/java/com/contrabass/mapleclassic/application/view/MapView.java
@@ -0,0 +1,15 @@
+package com.contrabass.mapleclassic.application.view;
+
+public class MapView {
+    public void printHenesys() {
+    }
+
+    public void printKerningCity() {
+    }
+
+    public void printPerion() {
+    }
+
+    public void printEllinia() {
+    }
+}
