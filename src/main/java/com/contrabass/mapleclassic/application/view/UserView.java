81459e2a5a2db5e5be2226cc57af2a026733d8ed
manje cho <whakswp123@naver.com>
2023-06-15 19:45:20 +0900

    특정 유저 정보 출력 메소드 추가 (#2)

--- src/main/java/com/contrabass/mapleclassic/application/view/UserView.java
+++ src/main/java/com/contrabass/mapleclassic/application/view/UserView.java
@@ -1,4 +1,9 @@
 package com.contrabass.mapleclassic.application.view;
 
 public class UserView {
+    public void printUserInfo(String nickName, int level, String job) {
+        System.out.println("★★★ " + nickName + " ★★★");
+        System.out.println("LEVEL : " + level);
+        System.out.println("직업 : " + job);
+    }
 }
