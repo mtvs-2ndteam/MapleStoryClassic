c6c7941f5e8b52b0619fd29ddfa8307e91bd44e1
manje cho <whakswp123@naver.com>
2023-06-15 19:44:23 +0900

    0~9 이외의 입력 예외 처리 추가 (#2)

---
+++ src/main/java/com/contrabass/mapleclassic/utils/MainException.java
@@ -0,0 +1,18 @@
+package com.contrabass.mapleclassic.utils;
+
+public class MainException {
+    public void validateInputValue(String readLine) {
+        if (!readLine.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("0~9 숫자만 입력하세요.");
+        }
+    }
+
+    public int solveInputValueException(String readLine) {
+        try {
+            validateInputValue(readLine);
+        } catch (IllegalArgumentException ie) {
+            return 0;
+        }
+        return Integer.parseInt(readLine);
+    }
+}
