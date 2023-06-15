cb148eb07918094a959679e09b3b3d63d8815b6e
manje cho <whakswp123@naver.com>
2023-06-15 19:47:26 +0900

    특정 회원 정보 조회 메소드 추가 (#2)

--- src/main/java/com/contrabass/mapleclassic/application/service/GameService.java
+++ src/main/java/com/contrabass/mapleclassic/application/service/GameService.java
@@ -1,4 +1,17 @@
 package com.contrabass.mapleclassic.application.service;
 
+import com.contrabass.mapleclassic.domain.service.UserDomainService;
+import org.springframework.stereotype.Service;
+
+import static com.contrabass.mapleclassic.Constant.CONTEXT;
+
+@Service
 public class GameService {
+    UserDomainService userDomainService = CONTEXT.getBean("userDomainService", UserDomainService.class);
+
+    // 특정 회원 정보 조회
+    public void selectOneUser(String userId) {
+//        userDomainService.checkUserId(userId);
+    }
+
 }
