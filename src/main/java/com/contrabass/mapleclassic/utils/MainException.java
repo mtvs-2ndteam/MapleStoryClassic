63b3485c3f02f8836a60873cf36e2691b76ec986
manje cho <whakswp123@naver.com>
2023-06-16 11:40:33 +0900

    입력 예외 처리 리턴값 변경 (#2)

--- src/main/java/com/contrabass/mapleclassic/utils/MainException.java
+++ src/main/java/com/contrabass/mapleclassic/utils/MainException.java
@@ -2,7 +2,7 @@ package com.contrabass.mapleclassic.utils;
 
 public class MainException {
     public void validateInputValue(String readLine) {
-        if (!readLine.matches("^[0-9]*$")) {
+        if (!readLine.matches("^[0-9]*?")) {
             throw new IllegalArgumentException("0~9 숫자만 입력하세요.");
         }
     }
@@ -11,7 +11,7 @@ public class MainException {
         try {
             validateInputValue(readLine);
         } catch (IllegalArgumentException ie) {
-            return 0;
+            return -1;
         }
         return Integer.parseInt(readLine);
     }
