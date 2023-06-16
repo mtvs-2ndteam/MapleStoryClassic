6aaa7a7aa988f2054df7557b3b8a4d5488f857c5
youyeul lee <yuyeul301@gmail.com>
2023-06-16 14:45:29 +0900

    플레이어 스텟 및 1차스킬 추가,레벨업 로직 추가 #9#15#16

---
+++ src/main/java/com/contrabass/mapleclassic/domain/entity/PlayerDTO.java
@@ -0,0 +1,34 @@
+package com.contrabass.mapleclassic.domain.entity;
+
+
+import lombok.*;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PlayerDTO {
+    private int level;
+    private int baseHp;
+    private int baseMp;
+    private int maxHp;
+    private int maxMp;
+    private int armor;
+    private int baseExp;
+    private int maxExp;
+    private int meso;
+
+    private int atk;
+    private int mAtk;
+    private int criPercent;
+    private int criDamage;
+    private int statPoint;
+    private int skillPoint;
+
+    private ArrayList<SkillDTO> skills;
+}
+
+
