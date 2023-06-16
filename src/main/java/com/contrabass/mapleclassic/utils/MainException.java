014cc48f0022d767e66a7c50cac383e330ecab2b
manje cho <whakswp123@naver.com>
2023-06-16 14:39:18 +0900

    입력 예외 정규표현식 수정 (#2)

--- src/main/java/com/contrabass/mapleclassic/utils/MainException.java
+++ src/main/java/com/contrabass/mapleclassic/utils/MainException.java
@@ -2,7 +2,10 @@ package com.contrabass.mapleclassic.utils;
 
 public class MainException {
     public void validateInputValue(String readLine) {
-        if (!readLine.matches("^[0-9]*?")) {
+        if (!readLine.matches("^(0|[1-9]+[0-9]*)$")
+                || readLine.equals("")
+                && (Integer.parseInt(readLine) >= 0
+                && Integer.parseInt(readLine) < 10)) {
             throw new IllegalArgumentException("0~9 숫자만 입력하세요.");
         }
     }
