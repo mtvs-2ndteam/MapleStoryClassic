c0bc8f383c82d7eb8afff740c841933a02ac74cf
manje cho <whakswp123@naver.com>
2023-06-16 10:01:52 +0900

    basecamp 주소 변경 (#2)

--- src/main/java/com/contrabass/mapleclassic/Constant.java
+++ src/main/java/com/contrabass/mapleclassic/Constant.java
@@ -1,6 +1,5 @@
 package com.contrabass.mapleclassic;
 
-import com.contrabass.mapleclassic.utils.contextconfiguration.mainContextConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,5 +7,6 @@ import java.util.Scanner;
 
 public class Constant {
     public final static Scanner SCANNER = new Scanner(System.in);
-    public final static ApplicationContext CONTEXT = new AnnotationConfigApplicationContext(mainContextConfiguration.class);
+    public final static ApplicationContext CONTEXT =
+            new AnnotationConfigApplicationContext("com.contrabass.mapleclassic");
 }
